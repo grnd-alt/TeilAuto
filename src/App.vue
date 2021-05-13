3fcfbee95dbde93c5d44b067ee83b5c0cb0a165c
grnd-alt <salim.belakkaf@gmx.de>
2021-05-13 18:03:05 +0200

    navBar init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-
+  
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TopBar @navChange ="navChanged" :active="2" :navs="['Developer','Projects','Contact']"/>
     <fileupload/>
   </div>
   
 </template>
 
 <script>
+import fileupload from './components/fileupload.vue';
+import TopBar from './components/topBar.vue';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import fileupload from './components/fileupload.vue'
+Vue.use(VueRouter);
 export default {
   name: 'App',
   components: {
-    fileupload
+    fileupload,
+    TopBar,
+  },
+  methods:{
+    navChanged:function(changedto){
+      window.location.href = changedto;
+      alert("changed to: "+changedto);
+    }
   }
 }
+
 </script>
 
 <style>
@@ -26,6 +39,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/topBar.vue
@@ -0,0 +1,58 @@
+
+<style scoped>
+#FieldWrapper{
+    margin-bottom:1em;
+    background-color: #333;
+    width:auto;
+    /* padding-left:5%; */
+    overflow:hidden;
+}
+.field{
+    font-size:1.25em;
+    color:#f2f2f2;
+    padding:0.5em;
+    transition-duration: 0.5s;
+}
+.field:hover{
+    background-color:#ddd;
+    color:black;
+    cursor:pointer;
+    transition-duration:0.25s;
+}
+.active{
+    background-color:#ddd;
+    color:black;
+}
+</style>
+<template>
+    <div id = "tobBarMain"> 
+        <div id = "FieldWrapper"> 
+            <a class = "field" v-on:click="navClick" v-for="item in navs" :key="item">
+                {{ item }}
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        navs:Array,
+        active:Number,
+    },
+    methods:{
+        navClick:function(event){
+            var active = document.getElementsByClassName("active");
+            if(active.length >= 1){
+                for (let i = 0; i < active.length;i++){
+                    active[i].classList.remove("active");
+                }
+            }
+            event.target.classList.add("active");
+            this.$emit("navChange",event.target.innerHTML);
+        }
+    }
+}
+
+</script>
